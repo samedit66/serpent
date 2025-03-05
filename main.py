cae137cffc5dbd1447234efe2e93c28d57bf3567
samedit66 <samedit66@yandex.ru>
2025-03-05 03:35:24 +0300

    clean

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ feature
     -- По умолчанию возвращает строку с описанием местоположения
     -- объекта в памяти.
     external "Java"
-    alias "com.eiffel.base.Any.out"
+    alias "com.eiffel.Any.out"
     end
 end
 
@@ -26,11 +26,13 @@ feature
     t: INTEGER
     local
         x: INTEGER
+        y: ARRAY[INTEGER]
     do
         x := (2 + 2) - (4 * 7) / 10
     end
 end
 
+class Y end
 
 class ARRAY [E]
 feature
@@ -61,23 +63,25 @@ feature
     age: INTEGER
     name: STRING
 feature
+    fuck do end
+
     make (a_name: STRING; a_age: INTEGER)
     do
         age := a_age
         name := a_name
     end
 
-    calculate: INTEGER
-    do
-        Result := 1 + 2 - 10 + Result
+    java_method (a, b: INTEGER): INTEGER
+        external "Java"
+        alias "com.eiffel.PERSON_UTILS.method"
+    end
 
-        if Result < 10 then
-            Result := 4
-        elseif Result = 254 then
-            Result := 8 + 1
-        else
-            Result := 10
-        end
+    calculate (a, b: INTEGER): INTEGER
+    local
+        i: INTEGER
+        flag: BOOLEAN
+    do
+        i := java_method (1, 10)
     end
 end
 
@@ -117,10 +121,59 @@ from serpent.tree.type_decl import ClassType
 from serpent.semantic_checker.symtab import GlobalClassTable, make_class_symtab
 from serpent.semantic_checker.type_check import make_codegen_class, check_types
 from serpent.semantic_checker.utils import pretty_print_node
-from serpent.codegen.constant_pool import make_constant_pool, add_package_prefix
+from serpent.codegen.constpool import make_const_pool, add_package_prefix
 from serpent.codegen.preprocess import make_general_class
 from serpent.codegen.class_file import make_class_file
 from serpent.codegen.genbytecode import generate_bytecode_for_stmts
+
+
+def pretty_print_program(commands: list, const_pool = None) -> None:
+    """
+    Выводит ассемблерное представление программы (список объектов ByteCommand)
+    с адресами, мнемониками, операндами и шестнадцатеричным представлением байтов.
+    
+    Если задана таблица констант (const_pool), то для команд, содержащих поле `index`,
+    выводится в комментарии строковое представление константы из таблицы.
+    
+    :param commands: Список объектов, наследующих ByteCommand.
+    :param const_pool: Объект ConstPool, содержащий константы для расшифровки.
+    """
+    offset = 0
+    for command in commands:
+        # Получаем байтовое представление команды
+        code_bytes = command.to_bytes()
+        # Форматируем смещение в виде 4-значного hex-числа
+        offset_str = f"{offset:04x}:"
+        # Используем имя класса в качестве мнемоники
+        mnemonic = command.__class__.__name__
+        
+        # Собираем операнды из полей dataclass (если они заданы)
+        operands = []
+        for field_name, field_def in getattr(command, '__dataclass_fields__', {}).items():
+            # Пропускаем служебное поле, если требуется (например, если tag вычисляется через cached_property)
+            value = getattr(command, field_name)
+            operands.append(f"{field_name}={value}")
+        operand_str = " " + ", ".join(operands) if operands else ""
+        
+        # Если команда имеет атрибут "index" и задана таблица констант,
+        # получаем соответствующую константу для вывода комментария.
+        comment = ""
+        if const_pool is not None and hasattr(command, "index"):
+            idx = getattr(command, "index")
+            # Предполагается, что константный pool нумеруется с 1, а список – с 0.
+            try:
+                constant = const_pool.get_by_index(idx - 1)
+                comment = f" ; {constant!r}"
+            except IndexError:
+                comment = " ; <не найдено в ConstPool>"
+        
+        # Формируем строку шестнадцатеричных байт
+        bytes_hex = " ".join(f"{b:02x}" for b in code_bytes)
+        # Вывод строки: смещение, мнемоника с операндами, байты и комментарий (если есть)
+        print(f"{offset_str} {mnemonic}{operand_str}    // {bytes_hex}{comment}")
+        
+        # Обновляем смещение для следующей команды
+        offset += len(code_bytes)
 
 
 flatten_class_mapping = {fcls.class_name: fcls for fcls in flatten_classes}
@@ -130,15 +183,18 @@ if not error_collector.ok():
     error_collector.show()
     sys.exit(1)
 
-from serpent.codegen.constant_pool import *
+from serpent.codegen.constpool import *
 
 general_class = make_general_class(tclasses)
 
-tc = tclasses[3]
-rest_classes = tclasses[:3] + tclasses[4:]
+tc = tclasses[0]
+rest_classes = tclasses[1:]
 
-class_file = make_class_file(tc, rest_classes)
-pretty_print_constant_pool(class_file.constant_pool)
+class_file = make_class_file(general_class, tclasses, entry_point_method=mangle_name("fuck", class_name="PERSON"))
+pretty_print_const_pool(class_file.constant_pool)
+
+#for i in range(1000):
+#    pretty_print_program(class_file.methods_table.methods[i].code.bytecode, class_file.constant_pool)
 
 class_file_code = class_file.to_bytes()
 with open("PERSON.class", mode="wb") as f:
--- test.java
+++
@@ -1,4 +0,0 @@
-public class Test {
-    int foo() { return 1; }   // OK
-    double foo() { return 1.0; }  // Ошибка: конфликт сигнатур в JVM
-}
